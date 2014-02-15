6ff013ed5c2ea51b3f39bfc624069a0325f2cce4
franciscofranco <franciscofranco.1990@gmail.com>
2014-02-15 16:27:36 +0000

    selinux: add a ugly workaround to bail early when selinux tries to free a socket that is still in use and it ends in a Kernel panic. These lines are from opensource.samsung.com. While this will most likely put a bandaid over this Kernel panic it does not fix the bug so it might occur somewhere else if the same conditions are met but since I have only seen panics from this hook then we may as well sleep in peace.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: poondog <markj338@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3569,6 +3569,12 @@ static int sock_has_perm(struct task_struct *task, struct sock *sk, u32 perms)
 		return 0;
 	}
 
+	if (unlikely(!sksec))
+	{
+		pr_debug(KERN_CRIT "[SELinux] sksec is NULL, socket is already freed. \n");
+		return -EINVAL;
+	}
+
 	if (sksec->sid == SECINITSID_KERNEL)
 		return 0;
 
